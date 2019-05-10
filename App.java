2627d9f7ac2a5c1fb992fd48db2dc26eac167075
Gopi1995krishna <31003383+Gopi1995krishna@users.noreply.github.com>
2019-05-10 13:05:29 +0530

    Update App.java

--- App.java
+++ App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Exelon" );
-	System.out.println( "my name is goip:" );
+	System.out.println( "my name is goip krishna:" );
 	
     }
 }
