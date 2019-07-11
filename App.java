b8754c109b4fad1658846e8cd4e26772c3c95794
Gopi1995krishna <31003383+Gopi1995krishna@users.noreply.github.com>
2019-07-11 10:01:25 +0530

    Update App.java

--- App.java
+++ App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World! Exelon" );
 	System.out.println( "my name is gopi krishna:" );
+	System.out.println( "my name is gopi krishna test for multi repos merge to sigle repo:" );
 	
     }
 }
