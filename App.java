fb3aa5b06138e62b6dc0f2ed01a5562db9058585
Gopi1995krishna <31003383+Gopi1995krishna@users.noreply.github.com>
2019-12-18 14:23:46 +0530

    Update App.java

--- App.java
+++ App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Exelon" );
+        System.out.println( "Hello World! Oracle" );
 	System.out.println( "my name is gopi krishna:" );
 	System.out.println( "my name is gopi krishna test for multi repos merge to sigle repo:" );
 	
